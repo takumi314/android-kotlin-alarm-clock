df52b1cec014dab78fbd0529c6c73db01882dfb5
takumi314 <isospin_symmetry@me.com>
2020-02-09 16:01:27 +0900

    make AlarmBroadcastReceiver inherited from BroadcastReceiver

---
+++ app/src/main/java/com/example/myalarmclock/AlarmBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.myalarmclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("AlarmBroadcastReceiver.onReceive() is not implemented")
+    }
+}
