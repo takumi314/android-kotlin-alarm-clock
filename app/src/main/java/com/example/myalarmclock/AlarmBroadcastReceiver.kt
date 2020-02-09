03beea63463aa28af67cd2f8cf7b040afa08c9c1
takumi314 <isospin_symmetry@me.com>
2020-02-09 16:05:09 +0900

    add comment

--- app/src/main/java/com/example/myalarmclock/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/myalarmclock/AlarmBroadcastReceiver.kt
@@ -4,8 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
+// アラームマネージャからの通知を受け取るクラス
 class AlarmBroadcastReceiver : BroadcastReceiver() {
 
+    // ブロードキャストインデントを受け取った時にコールされる
+    // ^ intent: 受け取ったインテントの情報を取得することができる
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         TODO("AlarmBroadcastReceiver.onReceive() is not implemented")
